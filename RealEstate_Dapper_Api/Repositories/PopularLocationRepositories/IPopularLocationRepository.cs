8ebde1e6230f27c18f3f44bf1dc6ae39ca3f406e
ahmthknpsa <ahmethakanpusa01@gmail.com>
2025-05-02 02:22:13 +0300

    PopularLocation kısmı tamamlandı

--- RealEstate_Dapper_Api/Repositories/PopularLocationRepositories/IPopularLocationRepository.cs
+++ RealEstate_Dapper_Api/Repositories/PopularLocationRepositories/IPopularLocationRepository.cs
@@ -1,4 +1,4 @@
-﻿using RealEstate_Dapper_Api.Dtos.BottomGridDtos;
+﻿
 using RealEstate_Dapper_Api.Dtos.PopularLocationDtos;
 
 namespace RealEstate_Dapper_Api.Repositories.PopularLocationRepositories
@@ -6,6 +6,10 @@ namespace RealEstate_Dapper_Api.Repositories.PopularLocationRepositories
     public interface IPopularLocationRepository
     {
         Task<List<ResultPopularLocationDto>> GetAllPopularLocationAsync();
-        
+        void CreatePopularLocation(CreatePopularLocationDto createPopularLocationDto);
+        void DeletePopularLocation(int id);
+        void UpdatePopularLocation(UpdatePopularLocationDto updatePopularLocationDto);
+        Task<GetByIDPopularLocationDto> GetPopularLocation(int id);
+
     }
 }
