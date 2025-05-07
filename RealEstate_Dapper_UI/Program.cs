a65d5206e13d9320cb9f882334c6a82a111d2aad
ahmthknpsa <ahmethakanpusa01@gmail.com>
2025-05-07 18:07:58 +0300

    Kullanıcı paneli işlemleri yapıldı

--- RealEstate_Dapper_UI/Program.cs
+++ RealEstate_Dapper_UI/Program.cs
@@ -22,7 +22,16 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Default}/{action=Index}/{id?}");
+
+    name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
+
 
 app.Run();
